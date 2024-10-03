34648000cc491c50f863f19156c7a25ede77144b
willianjohny05 <willianjohny05@gmail.com>
2024-10-02 21:19:45 -0300

    Novas novidades

--- App.js
+++ App.js
@@ -1,25 +1,49 @@
-import React,{useEffect} from 'react';
+import React,{useEffect, useState} from 'react';
 import * as SplashScreen from 'expo-splash-screen'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen'
 import LoginScreen from './screens/LoginScreen'
+import DespesasScreen from './screens/DespesasScreen';
+import ReceitasScreen from './screens/ReceitasScreen';
 
 const Stack = createStackNavigator();
 SplashScreen.preventAutoHideAsync();
 
 export default function App(){
+  const [isLoggedIn, SetIsLoggedIn] = useState(false)
+
   useEffect(() =>{
     setTimeout(async()=> {
       await SplashScreen.hideAsync();
     },3000)
   },[])
 
+
+
+  const handleLogin = () => {
+    SetIsLoggedIn(true)
+  }
+
+  
+  const handleLogout = () => {
+    SetIsLoggedIn(false)
+  }
+
+  if (!isLoggedIn){
+    return <LoginScreen onLogin = {handleLogin} />
+  }
+
   return(
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'>
-        <Stack.Screen name='Login' component={LoginScreen}/>
-        <Stack.Screen name='Home' component={HomeScreen}/>
+      <Stack.Navigator initialRouteName='Home'>
+      <Stack.Screen name='Home' />
+      {(props) => <HomeScreen {...props} onLogout={handleLogout}/>}</Stack.Screen>
+        
+
+
+        <Stack.Screen name='Receitas' component={ReceitasScreen} options={[title: "Receitas"]} />
+        <Stack.Screen name='Despesas' component={DespesasScreen} options={[title: "Despesas"]} />
       </Stack.Navigator>
     </NavigationContainer>
   )
---
+++ screens/ReceitasScreen.js
@@ -0,0 +1,10 @@
+import {Text, View} from 'react-native'
+
+
+export default function ReceitasScreen(){
+    return(
+        <View>
+            <Text>Tela de Receitas</Text>
+        </View>
+    )
+}
